bed63e7fb246434f74457d14e5c96d2e873331b8
enzovitale <enzovitale@outlook.com>
2020-09-04 12:05:16 +0200

    Added a deliberately failing unit test, to test continuos integration.

--- GreeterTest/GreeterTests.cs
+++ GreeterTest/GreeterTests.cs
@@ -12,5 +12,7 @@ namespace GreeterTest
             var g = new Greeter();
             Assert.IsNotEmpty(g.Greeting());
         }
+
+        [Test] public void GreeterTestPurposefullyFailing() => Assert.Fail();
     }
 }
